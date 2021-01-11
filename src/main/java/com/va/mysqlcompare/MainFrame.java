d4404099cfec52ba93803e93a4a6a6a475154bc6
Daniel Volk <mail@volkarts.com>
2021-01-11 14:16:53 +0100

    Fix imports

--- src/main/java/com/va/mysqlcompare/MainFrame.java
+++ src/main/java/com/va/mysqlcompare/MainFrame.java
@@ -16,24 +16,7 @@
 package com.va.mysqlcompare;
 
 import com.va.securestore.Store;
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import javax.swing.AbstractButton;
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.plaf.basic.BasicButtonUI;
 import org.slf4j.LoggerFactory;
 
 public class MainFrame extends javax.swing.JFrame
